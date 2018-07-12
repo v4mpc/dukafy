de4e1c3f994a0d940db81760fcc952ef588705c9
v4mpc <yona101992@gmail.com>
2018-07-12 18:03:55 +0300

    apikesdfdsffsfdsfdsfdsfddfdssdfyfg

--- resources/views/partials/_topbar.blade.php
+++ resources/views/partials/_topbar.blade.php
@@ -6,7 +6,9 @@
         <li class="nav-item mr-auto">
           <a class="navbar-brand" href="index-2.html">
             {{-- <img class="brand-logo" alt="modern admin logo" src="../../../app-assets/images/logo/logo.png"> --}}
+            @if(count(App\Setting::all()))
           <h3 class="brand-text">{{$settings->store_name}}</h3>
+          @endif
           </a>
         </li>
         <li class="nav-item d-none d-md-block float-right"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i class="toggle-icon ft-toggle-right font-medium-3 white" data-ticon="ft-toggle-right"></i></a></li>
