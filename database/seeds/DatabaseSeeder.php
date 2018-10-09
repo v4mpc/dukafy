5930c9303eeaaa03692a207b4be754e28a7eb45a
v4mpc <yona101992@gmail.com>
2018-10-09 14:00:43 +0300

    bugs

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,9 +15,9 @@ class DatabaseSeeder extends Seeder
         // $this->call(CategoriesTableSeeder::class);
         // $this->call(ProductsTableSeeder::class);
         // $this->call(ProductImagesTableSeeder::class);
-        //  $this->call(UserTableSeeder::class);
+        $this->call(UserTableSeeder::class);
         // $this->call(SliderImagesSeeder::class);
-        $this->call(PackagesTableSeeder::class);
-        $this->call(AccountTableSeeder::class);
+        // $this->call(PackagesTableSeeder::class);
+        // $this->call(AccountTableSeeder::class);
     }
 }
