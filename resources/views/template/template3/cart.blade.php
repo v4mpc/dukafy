992919038f04adc83aff72509f4ff16d215bdf84
v4mpc <yona101992@gmail.com>
2018-11-29 15:20:02 +0300

    order working for template3

--- resources/views/template/template3/cart.blade.php
+++ resources/views/template/template3/cart.blade.php
@@ -149,7 +149,7 @@
                                 <textarea class="form-control unicase-form-control text-input" name="comment" id="" cols="30" rows="5" placeholder="Special Note of delivery..."></textarea>
                                 
                               </div>
-                          <button type="button"  class="btn-upper btn btn-primary">Place Order</button>
+                          <button type="submit"  class="btn-upper btn btn-primary">Place Order</button>
                         </form>
                     </div>	
                     <!-- already-registered-login -->		
