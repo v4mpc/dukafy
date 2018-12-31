a038b53e613d5bf7edaf5b0ba821359844d37e20
v4mpc <yona101992@gmail.com>
2018-12-31 10:46:27 +0300

    added character limit on product name template 3

--- resources/views/template/template3/search.blade.php
+++ resources/views/template/template3/search.blade.php
@@ -101,7 +101,8 @@
 
 
 <div class="product-info text-left">
-<h5 class="name"><a href="{{route('product.show',$product->id)}}">{{$product->name}}</a></h5>
+<h5 class="hidden-xs hidden-sm"><a href="{{route('product.show',$product->id)}}" title="{{$product->name}}">{{strlen($product->name)>20?str_limit($product->name,20):$product->name}}</a></h5>
+                            <h5 class="hidden-md hidden-lg"><a href="{{route('product.show',$product->id)}}" title="{{$product->name}}">{{strlen($product->name)>10?str_limit($product->name,10):$product->name}}</a></h5>
 
 <div class="description"></div>
 
