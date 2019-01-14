2f5ae1245f9a5f38a50f475e939b47b0ad1f591a
v4mpc <yona101992@gmail.com>
2019-01-14 15:17:15 +0300

    mark as read notification

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Order;
 use App\Http\Resources\Orders as OrdersResource;
 use App\Http\Resources\Order as OrderResource;
+use JWTAuth;
 
 class OrderController extends Controller
 {
@@ -17,6 +18,12 @@ class OrderController extends Controller
 
     public function show($account_id, $order_id)
     {
+        foreach (JWTAuth::parseToken()->toUser()->unreadNotifications as $notification) {
+            if ($notification->data['order_id']==$order_id) {
+                $notification->markAsRead();
+                break;
+            }
+        }
         return OrderResource::collection(Order::withoutGlobalScopes()->where('account_id', $account_id)->where('id', $order_id)->get());
     }
 }
