7cacb1d268f74294c043027b50e4e4d7a3427b42
v4mpc <yona101992@gmail.com>
2019-01-21 14:57:57 +0300

    bug fix

--- resources/views/maintanance.blade.php
+++ resources/views/maintanance.blade.php
@@ -24,8 +24,8 @@
                         <div class="card border-grey border-lighten-3 px-1 py-1 box-shadow-3 m-0">
                             <div class="card-body">
                                 <span class="card-title text-center">
-                            <img src="{{asset('images/logo.png')}}" class="img-fluid mx-auto d-block pt-2"
-                            width="250" alt="logo">
+                            {{-- <img src="{{asset('images/logo.png')}}" class="img-fluid mx-auto d-block pt-2"
+                            width="250" alt="logo"> --}}
                           </span>
                             </div>
                             <div class="card-body text-center">
