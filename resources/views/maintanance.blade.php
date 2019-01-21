e6002e0a127d1f7deb23094b62c4bc14bf9a7775
v4mpc <yona101992@gmail.com>
2019-01-21 14:56:16 +0300

    bug fix

--- resources/views/maintanance.blade.php
+++ resources/views/maintanance.blade.php
@@ -37,7 +37,7 @@
                           
                             <hr>
                             <p class="socialIcon card-text text-center pt-2 pb-2">
-                                <a href="#" class="btn btn-social-icon mr-1 mb-1 btn-outline-facebook">
+                                <a href="https://dukafy.co.tz" class="mr-1 mb-1 btn-outline-facebook">
                             Powered By Dukafy
                           </a>
                             </p>
