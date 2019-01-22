3ab5ed4490680f6a9a31f879597244b043ec2184
v4mpc <yona101992@gmail.com>
2019-01-22 14:52:53 +0300

    dukafy domain fixed

--- app/Http/Middleware/CheckDomain.php
+++ app/Http/Middleware/CheckDomain.php
@@ -17,16 +17,14 @@ class CheckDomain
      */
     public function handle($request, Closure $next)
     {
-        if (!$request->is('api/*')) {
+        if (!$request->is('api/*')||$request->getHost()!="dukafy.co.tz") {
             $account=0;
             if ($request->getHost()!="adshlits.dukafy.co.tz") {
                 $account=Account::where('id', getAccountId($request))->where('status', 1)->first();
             }
-        
             if ($account || $request->getHost()=="adshlits.dukafy.co.tz") {
                 return $next($request);
             }
-    
             return abort(404);
         }
         return $next($request);
