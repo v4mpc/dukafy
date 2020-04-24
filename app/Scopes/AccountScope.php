5a0eeecbdff537a1c94b0f9a428aeab7259a2b37
Yona Mahundi <yona_mahundi@tz.jsi.com>
2020-04-24 17:32:55 +0300

    testing

--- app/Scopes/AccountScope.php
+++ app/Scopes/AccountScope.php
@@ -30,25 +30,25 @@ class AccountScope implements Scope
         //lets check if request if from mobile
         //if its from mobile
         //we make we only check if its account id is greater than 1
-        $domain=preg_replace('/www\./', "", Request::getHost());
-        $domain=preg_replace('/www\./', "", $domain);
-        if (Request::is('api/*') || strpos(php_sapi_name(), 'cli') !== false) {
-            $builder->where('account_id', '>', 1);
-        } else {
-            if (Request::getHost()=="adshlits.dukafy.co.tz" ||$domain=='dukafy.co.tz'||$domain=='dukafy.local') {
-                $account_id=1;
-            } else {
-                if (strpos($domain, 'dukafy')!==false) {
-                    $domain_array=explode('.', $domain);
-                    $subdomain=$domain_array[0];
-                    $account_id=Account::where('subdomain', $subdomain)->first()->id;
-                } else {
+        // $domain=preg_replace('/www\./', "", Request::getHost());
+        // $domain=preg_replace('/www\./', "", $domain);
+        // if (Request::is('api/*') || strpos(php_sapi_name(), 'cli') !== false) {
+        //     $builder->where('account_id', '>', 1);
+        // } else {
+        //     if (Request::getHost()=="adshlits.dukafy.co.tz" ||$domain=='dukafy.co.tz'||$domain=='dukafy.local') {
+        //         $account_id=1;
+        //     } else {
+        //         if (strpos($domain, 'dukafy')!==false) {
+        //             $domain_array=explode('.', $domain);
+        //             $subdomain=$domain_array[0];
+        //             $account_id=Account::where('subdomain', $subdomain)->first()->id;
+        //         } else {
                     
-                    $account_id=Account::where('domain', $domain)->first()->id;
+        //             $account_id=Account::where('domain', $domain)->first()->id;
                     
-                }
-            }
-            $builder->where('account_id', $account_id);
-        }
+        //         }
+        //     }
+        //     $builder->where('account_id', $account_id);
+        // }
     }
 }
