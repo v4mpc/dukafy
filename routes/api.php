4e984c239877c542ec485057efb13faaaa5b7177
Yona Mahundi <yona_mahundi@tz.jsi.com>
2020-05-08 22:12:07 +0300

    minor bug fixes

--- routes/api.php
+++ routes/api.php
@@ -12,49 +12,43 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
-
-
+ */
 
 Route::get('whois/{domain}', function ($domain) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, 'http://mydomains.dukafy.co.tz/includes/api.php');
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt(
-    $ch,
-    CURLOPT_POSTFIELDS,
-    http_build_query(
-        array(
-            'action' => 'DomainWhois',
-            // See https://developers.whmcs.com/api/authentication
-            'username' => 'admin',
-            'password' => md5('B5r9gFuu87EDPemF'),
-            'domain' => $domain.".co.tz",
-            'responsetype' => 'json',
+        $ch,
+        CURLOPT_POSTFIELDS,
+        http_build_query(
+            array(
+                'action' => 'DomainWhois',
+                // See https://developers.whmcs.com/api/authentication
+                'username' => 'admin',
+                'password' => md5('B5r9gFuu87EDPemF'),
+                'domain' => $domain . ".co.tz",
+                'responsetype' => 'json',
+            )
         )
-    )
-);
+    );
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $response = curl_exec($ch);
     echo $response;
     curl_close($ch);
 });
 
-
-
-
 //mobile api shall go here
 
 //
 
-
 Route::post('mobile/login', 'API\AuthController@login')->name('login');
-Route::get('mobile/needs_update',function(Request $request){
-    if($request->version_number!=Config::get('app.version')&&$request->version_number!='1.0.5'){
-        #return true we require an update
-        
-        return response()->json('true');
-    }
+Route::get('mobile/needs_update', function (Request $request) {
+    // if($request->version_number!=Config::get('app.version')&&$request->version_number!='1.0.5'){
+    //     #return true we require an update
+
+    //     return response()->json('true');
+    // }
     #return false no update is required
     return response()->json('false');
 
@@ -75,5 +69,5 @@ Route::group(['prefix' => 'mobile', 'middleware' => 'jwt.auth'], function () {
     Route::get('orders/{account_id}/{id}', 'API\OrderController@show');
     Route::get('featured/{account_id}/{id}', 'API\ProductController@toggleFeatured');
     Route::get('stock/{account_id}/{id}', 'API\ProductController@toggleOutStock');
- 
+
 });
